6396d390394e55124181ebe71f4e858b4a57e9d7
DmitVar <dmitvar@gmail.com>
2023-01-19 22:49:13 +0300

    Change Menu.scss, Menu.js, PaymentPage.scss

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -13,50 +13,59 @@ const Menu = (props) => {
 		setTabletmenuVisible(tabletMenuVisible => false);
 	}
 	return (
-		<div className="menu-container">
-			<div className='menu-container__logo'>
-				<p className='logo'>мария бурибо</p>
-				<p className='logo__profession'>психолог</p>
-			</div>
-			<div className='navigation__container'>
-				<nav className='navigation descktop_visible'>
-					<ul>
-						<li><a href='#about'>Обо мне</a></li>
-						<li><a href='#paymentPage'>Стоимость</a></li>
-						<li><a href='#faqPage'>FAQ</a></li>
-						<li><a href='#contacts'>Контакты</a></li>
-					</ul>
-				</nav>
-				<p className='navigation__phone'>
-					+375 (44) 793-53-68
-				</p>
-				<nav className="tablet__navigation">
-					<button type='button' onClick={showTabletMenu}><img src={burger} alt="burger" /></button>
-					<div className={tabletMenuVisible ? "tablet__menu" : "tablet__menu tablet__menu_visible"}>
-						<div className="menu-container__tablet">
-							<div className='menu-container__logo'>
-								<p className='logo'>мария бурибо</p>
-								<p className='logo__profession'>психолог</p>
-							</div>
-							<p className='navigation__phone'>
-								+375 (44) 793-53-68
-							</p>
-							<button type='button' onClick={hidesTabletMenu}><img src={closeButtonMenu} alt="burger" /></button>
-						</div>
+		<div>
+			<div className="menu-container descktop__navigtion">
+				<div className='menu-container__logo'>
+					<p className='logo'>мария бурибо</p>
+					<p className='logo__profession'>психолог</p>
+				</div>
+				<div className='navigation__container'>
+					<nav className='navigation descktop_visible'>
 						<ul>
-							<li onClick={hidesTabletMenu}><a href='#about'>Обо мне</a></li>
-							<li onClick={hidesTabletMenu}><a href='#paymentPage'>Стоимость</a></li>
-							<li onClick={hidesTabletMenu}><a href='#faqPage'>FAQ</a></li>
-							<li onClick={hidesTabletMenu}><a href='#contacts'>Контакты</a></li>
+							<li><a href='#about'>Обо мне</a></li>
+							<li><a href='#paymentPage'>Стоимость</a></li>
+							<li><a href='#faqPage'>FAQ</a></li>
+							<li><a href='#contacts'>Контакты</a></li>
 						</ul>
-						<div className="socialIcon__box">
-							<a href='#'><div className="docialIcon instagram"></div></a>
-							<div className="docialIcon viber"></div>
-							<div className="docialIcon telegram"></div>
-						</div>
-					</div>
-				</nav>
+					</nav>
+					<p className='navigation__phone'>
+						+375 (44) 793-53-68
+					</p>
+				</div>
 			</div>
+			<div className={tabletMenuVisible ? "menu-container__big tablet__navigation" : "menu-container tablet__navigation"}>
+				<div className="tablet__viseble-container">
+					<div className='menu-container__logo'>
+						<p className='logo'>мария бурибо</p>
+						<p className='logo__profession'>психолог</p>
+					</div>
+					<p className='navigation__phone'>
+						+375 (44) 793-53-68
+					</p>
+					<div className="button__container">
+						<button className='menu__button' onClick={tabletMenuVisible ? hidesTabletMenu : showTabletMenu}>
+							<img src={tabletMenuVisible ? closeButtonMenu : burger} alt="" />
+						</button>
+					</div>
+				</div>
+				<div className='navigation__container'>
+					<nav className='navigation descktop_visible'>
+						<ul>
+							<li><a href='#about' onClick={hidesTabletMenu}>Обо мне</a></li>
+							<li><a href='#paymentPage' onClick={hidesTabletMenu}>Стоимость</a></li>
+							<li><a href='#faqPage' onClick={hidesTabletMenu}>FAQ</a></li>
+							<li><a href='#contacts' onClick={hidesTabletMenu}>Контакты</a></li>
+						</ul>
+					</nav>
+					<div className="navigation__socialIcon__box">
+						<a href='#'><div className="docialIcon instagram"></div></a>
+						<div className="docialIcon viber"></div>
+						<div className="docialIcon telegram"></div>
+					</div>
+				</div>
+
+			</div>
+
 		</div>
 	)
 }
