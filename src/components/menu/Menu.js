6d4451df1363a3f6295571c2a5c247477d890ab6
DmitVar <dmitvar@gmail.com>
2023-01-21 16:41:26 +0300

    Change file: menu.scss, paymentPage-all, faqpage.scss

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -71,6 +71,42 @@ const Menu = (props) => {
 				</div>
 
 			</div>
+			<div className="phone">
+				<div className={tabletMenuVisible ? "menu-container__big tablet__navigation" : "menu-container tablet__navigation"}>
+					<div className="tablet__viseble-container">
+						<div className='menu-container__logo'>
+							<p className='logo'>мария бурибо</p>
+							<p className='logo__profession'>психолог</p>
+						</div>
+						<div className="button__container">
+							<button className='menu__button' onClick={tabletMenuVisible ? hidesTabletMenu : showTabletMenu}>
+								<img src={tabletMenuVisible ? closeButtonMenu : burger} alt="" />
+							</button>
+						</div>
+					</div>
+					<div className='navigation__container'>
+						<nav className='navigation descktop_visible'>
+							<ul>
+								<li><a href='#about' onClick={hidesTabletMenu}>Обо мне</a></li>
+								<li><a href='#paymentPage' onClick={hidesTabletMenu}>Стоимость</a></li>
+								<li><a href='#faqPage' onClick={hidesTabletMenu}>FAQ</a></li>
+								<li><a href='#contacts' onClick={hidesTabletMenu}>Контакты</a></li>
+							</ul>
+						</nav>
+						<p className='navigation__phone'>
+							+375 (44) 793-53-68
+						</p>
+
+						<div className="navigation__socialIcon__box">
+							<a href='#'><div className="docialIcon instagram"></div></a>
+							<div className="docialIcon viber"></div>
+							<div className="docialIcon telegram"></div>
+						</div>
+					</div>
+
+				</div>
+
+			</div>
 		</div>
 	)
 }
