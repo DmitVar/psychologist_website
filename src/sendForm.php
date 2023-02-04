ead63960c91331a2149410e583bc594f3f3dfb45
DmitVar <dmitvar@gmail.com>
2023-02-04 10:45:41 +0300

    Change php file

--- src/sendForm.php
+++ src/sendForm.php
@@ -23,37 +23,37 @@ $body = "
 // Настройки PHPMailer
 $mail = new PHPMailer\PHPMailer\PHPMailer();
 try {
-    $mail->isSMTP();   
+    $mail->isSMTP();
     $mail->CharSet = "UTF-8";
     $mail->SMTPAuth   = true;
     //$mail->SMTPDebug = 2;
-    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+    $mail->Debugoutput = function ($str, $level) {
+        $GLOBALS['status'][] = $str;
+    };
 
     // Настройки вашей почты
-    $mail->Host       = 'ssl://smtp.mail.ru'; // SMTP сервера вашей почты
+    $mail->Host       = 'smtp.mail.ru'; // SMTP сервера вашей почты
     $mail->Username   = 'mpsihologist@mail.ru'; // Логин на почте
-    $mail->Password   = 'ty5KjwkGqryNeJmD8znF'; // Пароль на почте 07M11Psiholog
-    $mail->SMTPSecure = 'ssl';
-    $mail->Port       = 465;
+    $mail->Password   = 'Rysgvb4xCrniK3yEj7SB'; // Пароль на почте 31M01Bravo
     $mail->setFrom('mpsihologist@mail.ru', 'Заявка с сайта'); // Адрес самой почты и имя отправителя
 
     // Получатель письма
-    $mail->addAddress('buribo.maria@mail.ru');
+    $mail->addAddress('dmitvar@gmail.com');
 
-// Отправка сообщения
-$mail->isHTML(true);
-$mail->Subject = $title;
-$mail->Body = $body;    
+    // Отправка сообщения
+    $mail->isHTML(true);
+    $mail->Subject = $title;
+    $mail->Body = $body;
 
-// Проверяем отравленность сообщения
-if ($mail->send()) {$result = "success";} 
-else {$result = "error";}
-
+    // Проверяем отравленность сообщения
+    if ($mail->send()) {
+        $result = "success";
+    } else {
+        $result = "error";
+    }
 } catch (Exception $e) {
     $result = "error";
     $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
 }
 
-// Отображение результата
-echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
-?>
+header('https://www.onliner.by/');
